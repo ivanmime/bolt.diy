d3ea4cd60ac4aa96cb46c099fc788b984dd1af41
ivanmime <imiragaya03@gmail.com>
2025-07-19 22:10:45 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
+import remix from '@remix-run/dev/vite';
 
 export default defineConfig({
+  plugins: [remix()],
   server: {
     port: 5173,
     host: true,
-    allowedHosts: ['bolt.pinseo.es'],
-  },
+    allowedHosts: ['bolt.pinseo.es']
+  }
 });
