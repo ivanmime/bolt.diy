b5f99ed6f0884054439087143115c7887c2e28a7
ivanmime <imiragaya03@gmail.com>
2025-07-19 22:53:23 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
+import remix from '@remix-run/dev/vite';
 
 export default defineConfig({
+  plugins: [remix()],
   server: {
     port: 3000,
     host: '0.0.0.0',
